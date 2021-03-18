3c22863f8b886a48da7da4b969d957e6c7d98c0d
hourglasshoro <hourglasshoro.628@gmail.com>
2021-03-18 15:15:54 +0900

    Update 事業者の管理画面を追加

--- src/pages/provider-register.tsx
+++ src/pages/provider-register.tsx
@@ -7,6 +7,7 @@ const hostUrl = "http://localhost:3000"
 const ProviderRegister: React.VFC<null> = () => {
   const router = useRouter()
 
+  // HACK: StripeのSDK(import Stripe from "stripe")でうまく送信できないのでaxiosを使用
   // StripeのAPI用のインスタンスを生成
   const axiosInstance = axios.create({
     baseURL: hostUrl,
@@ -16,6 +17,7 @@ const ProviderRegister: React.VFC<null> = () => {
     timeout: 2000
   })
 
+  // アカウントを作成するためのリンクを取得して遷移させる
   const createAccount = async () => {
     try {
       // アカウント作成
@@ -23,12 +25,13 @@ const ProviderRegister: React.VFC<null> = () => {
         type: "express",
         country: "JP"
       }
+
       let axiosParams = new URLSearchParams()
       Object.keys(createParams).forEach((key) => {
         axiosParams.append(key, createParams[key])
       })
-
       let response = await axiosInstance.post("https://api.stripe.com/v1/accounts", axiosParams)
+      //TODO: 全体を状態管理しているやつにstripeのアカウントIDを保存
 
       // アカウント登録リンク生成
       //TODO: refresh_urlとreturn_urlを設定
@@ -52,9 +55,41 @@ const ProviderRegister: React.VFC<null> = () => {
     }
   }
 
+  const getDashboardUrl = async () => {
+    try {
+      //TODO: DBからfirebase idに紐づいているstripe idをとってくる, 下のidは入れ替え
+      const id = "acct_1IWDvIPtFr4bVSKg"
+
+      const response = await axiosInstance.post(
+        `https://api.stripe.com/v1/accounts/${id}/login_links`
+      )
+      await router.push(response.data.url)
+    } catch (e) {
+      console.log(e)
+      // TODO: エラーハンドリング
+    }
+  }
+
   return (
     <div>
-      <button onClick={() => createAccount()}>アカウントを連携する</button>
+      <div className="inline-block mr-2 mt-2">
+        <button
+          type="button"
+          className="focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-blue-500 hover:bg-blue-600 hover:shadow-lg"
+          onClick={() => createAccount()}
+        >
+          アカウントを連携する
+        </button>
+      </div>
+      <div className="inline-block mr-2 mt-2">
+        <button
+          type="button"
+          className="focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-green-500 hover:bg-green-600 hover:shadow-lg"
+          onClick={() => getDashboardUrl()}
+        >
+          管理画面を開く
+        </button>
+      </div>
     </div>
   )
 }
