48a22c5c0014d0c0137e1a14df832a382b805e50
Stickers-hackathon <watanabeso@crunchtimer.jp>
2021-03-20 20:52:52 +0900

    refs:tailwind色修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const colors = require("tailwindcss/colors")
-
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
@@ -7,8 +5,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "light-blue": colors.lightBlue,
-        cyan: colors.cyan,
         orange: "#F98A2C",
         yellow: "#FFB829",
         pink: "#E53170",
