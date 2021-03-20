9f3156d72f400cbf7a922d5683edfb004a6a3f9b
hourglasshoro <hourglasshoro.628@gmail.com>
2021-03-21 08:00:58 +0900

    Update トリミングモーダル

--- src/pages/test/trimModal.tsx
+++ src/pages/test/trimModal.tsx
@@ -1,10 +1,13 @@
 import { TrimModal } from "../../components/modal/trimModal"
 import Trim from "../trimming"
+import React from "react"
 
 const Index: React.VFC<{}> = () => {
+  const image = "/image/hero.png"
+
   return (
     <div>
-      <TrimModal isOpen={true} onButtonClick={() => {}} isDisabled={false}>
+      <TrimModal isOpen={true} onButtonClick={() => {}} isDisabled={false} image={image}>
         <Trim />
       </TrimModal>
     </div>
