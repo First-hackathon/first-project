a21bc533b80b4d1528533199803350d07c2b9bb3
TJ-Yusuke <jeeproud@gmail.com>
2021-03-21 03:29:00 +0900

    add: mypcページ実装

--- src/pages/my-pc-page/index.tsx
+++ src/pages/my-pc-page/index.tsx
@@ -4,23 +4,27 @@ import Image from "next/image"
 import { SupportersList } from "../../components/SupportersList"
 import { SupporterMock } from "../test/supportersList"
 import { RoundedButton, RoundedDivSize } from "../../components/Button/RoundButton"
+import { SnsShareButton, SnsType } from "../../components/Button/snsShare"
 
 const Index: React.VFC = () => {
   return (
     <>
       <Header />
       <section className="container mx-auto">
-        <div className="mt-5 mb-14 text-center">
-          <h1>あなたのPC</h1>
+        <div className="mt-32 mb-14 text-center">
+          <h1 className="text-3xl font-bold tracking-wide mb-8">あなたのPC</h1>
           {/* PC */}
-          <p className="text-gray-300 text-sm tracking-wider">
+          {/* <Image src="ここにfirebaseのURLが入る" /> */}
+          <p className="text-gray text-sm tracking-wider mt-4 mb-6">
             シェアをして応援してもらおう！ <br />
             画像をダウンロードすることもできます
           </p>
-          <div className="flex justify-around">
-            <Image src={"/icon/twitterShareIcon.svg"} width={40} height={40} />
-            <Image src={"/icon/shareUrlIcon.svg"} width={40} height={40} />
-            <Image src={"/icon/downloadIcon.svg"} width={40} height={40} />
+          <div className="flex justify-center">
+            {/* <Image src={"/icon/twitterShareIcon.svg"} width={40} height={40} /> */}
+            {/* TODO: ここにPCIDから生成したURLを入れる */}
+            <SnsShareButton url="/icon/twitterShareIcon.svg" snsType={SnsType.Twitter} />
+            {/* <Image src={"/icon/shareUrlIcon.svg"} width={40} height={40} /> */}
+            {/* <Image src={"/icon/downloadIcon.svg"} width={40} height={40} /> */}
           </div>
         </div>
         <SupportersList users={SupporterMock} />
