18c246d737310adc6c2f79ccd93f0457870503db
hourglasshoro <hourglasshoro.628@gmail.com>
2021-03-21 00:25:25 +0900

    Update tailwindの設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 const colors = require("tailwindcss/colors")
-
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   plugins: [],
   theme: {
     extend: {
-      // example
       colors: {
-        "light-blue": colors.lightBlue,
-        cyan: colors.cyan
+        orange: "#F98A2C",
+        yellow: "#FFB829",
+        pink: "#E53170",
+        gray: "#737373"
+      },
+      lineHeight: {
+        16: "4rem",
+        20: "5rem"
       }
     }
   }
