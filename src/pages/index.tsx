846000541bae8d09efc773d5893ebf74494f767f
hourglasshoro <hourglasshoro.628@gmail.com>
2021-03-21 00:26:11 +0900

    Update トップページのデザイン追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,98 +1,137 @@
 import React from "react"
+import Image from "next/image"
+import Header from "../components/header"
 
 const Index: React.VFC<null> = () => {
   return (
-    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl" />
-        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-          <div className="max-w-md mx-auto">
-            <div className="divide-y divide-gray-200">
-              <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                <p>
-                  An advanced online playground for Tailwind CSS, including support for things like:
-                </p>
-                <ul className="list-disc space-y-2">
-                  <li className="flex items-start">
-                    <span className="h-6 flex items-center sm:h-7">
-                      <svg
-                        className="flex-shrink-0 h-5 w-5 text-cyan-500"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </span>
-                    <p className="ml-2">
-                      Customizing your
-                      <code className="text-sm font-bold text-gray-900">
-                        tailwind.config.js
-                      </code>{" "}
-                      file
-                    </p>
-                  </li>
-                  <li className="flex items-start">
-                    <span className="h-6 flex items-center sm:h-7">
-                      <svg
-                        className="flex-shrink-0 h-5 w-5 text-cyan-500"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </span>
-                    <p className="ml-2">
-                      Extracting classes with
-                      <code className="text-sm font-bold text-gray-900">@apply</code>
-                    </p>
-                  </li>
-                  <li className="flex items-start">
-                    <span className="h-6 flex items-center sm:h-7">
-                      <svg
-                        className="flex-shrink-0 h-5 w-5 text-cyan-500"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </span>
-                    <p className="ml-2">Code completion with instant preview</p>
-                  </li>
-                </ul>
-                <p>
-                  Perfect for learning how the framework works, prototyping a new idea, or creating
-                  a demo to share online.
-                </p>
+    <>
+      <section className="relative w-full text-white">
+        <Image src="/image/hero.png" width={1920} height={1080} />
+
+        <div className="lg:mx-auto px-5">
+          <Header />
+
+          <div className="absolute top-0 xl:mt-72 lg:mt-60 mt-32 px-20 container xl:ml-20">
+            <h2 className="xl:text-7xl lg:text-5xl md:text-3xl font-semibold xl:leading-20 lg:leading-16 xl:mb-16 lg:mb-10 mb-5">
+              クリエイターのPCに
+              <br />
+              ステッカーを貼って応援
+            </h2>
+
+            <p className="lg:leading-8 mb-8 xl:text-xl lg:text-md text-sm">
+              コーヒー１杯分を贈る感覚で、好きなクリエイターを応援
+              <br />
+              することができます。
+            </p>
+
+            {/*TODO: Googleサインインに飛ばす*/}
+            <a className="xl:w-60 w-48 flex items-center" href={""}>
+              <Image src="/image/google-signin.svg" width={1280} height={308} />
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container mx-auto px-5 flex justify-between items-center my-20">
+          <div className="mt-10 w-6/12">
+            <div className="xl:text-6xl lg:text-5xl md:text-3xl font-semibold xl:leading-20 lg:leading-16 lg:mb-10 mb-5">
+              ファンからステッカー
+              <br />
+              を贈る
+            </div>
+
+            <div className="xl:text-xl lg:text-md text-sm text-sm">
+              <p>
+                クリエイターに向けて「ステッカー」という形のファンから小さい応援を贈ることができます。クリエイターはステッカーを貼ってもらうPCをバーチャルでつくるだけで始めることができます。
+              </p>
+            </div>
+          </div>
+
+          <div className="w-5/12">
+            <Image src="/image/pc-sticker.png" width={742} height={505} />
+          </div>
+        </div>
+      </section>
+
+      <section className="flex flex-col justify-center">
+        <Image src="/decorate/wave-white2orange.png" width={1920} height={140} />
+        {/*description*/}
+        <div className="bg-orange text-white">
+          <div className="container mx-auto px-5 flex justify-between items-center">
+            <div className="mt-10 w-6/12">
+              <div className="xl:text-6xl lg:text-5xl md:text-3xl font-semibold xl:leading-20 lg:leading-16 lg:mb-32 mb-15">
+                Stickersで
+                <br />
+                できること
               </div>
-              <div className="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
-                <p>Want to dig deeper into Tailwind?</p>
-                <p>
-                  <a
-                    href="https://tailwindcss.com/docs"
-                    className="text-cyan-600 hover:text-cyan-700"
-                  >
-                    {" "}
-                    Read the docs &rarr;{" "}
-                  </a>
-                </p>
+
+              {/*start fan*/}
+              <div className="lg:mb-32 mb-15">
+                <div className="xl:text-4xl lg:text-3xl md:text-xl font-semibold lg:mb-20 mb-10">
+                  ファン
+                </div>
+
+                <div className="lg:mb-20 mb-10">
+                  <div className="xl:text-3xl lg:text-2xl md:text-md font-semibold lg:mb-10 mb-5">
+                    ステッカーを貼ることで応援
+                  </div>
+
+                  <div className="xl:text-xl lg:text-md text-sm text-sm">
+                    <p>
+                      好きなクリエイターのバーチャルPCにステッカーを貼って応援しよう。ワンコインから購入して貼ることができます。{" "}
+                    </p>
+                  </div>
+                </div>
+
+                <div className="lg:mb-20 mb-10">
+                  <div className="xl:text-3xl lg:text-2xl md:text-md font-semibold lg:mb-10 mb-5">
+                    ステッカーを貼ることで応援
+                  </div>
+
+                  <div className="xl:text-xl lg:text-md text-sm text-sm">
+                    <p>
+                      好きなクリエイターのバーチャルPCにステッカーを貼って応援しよう。ワンコインから購入して貼ることができます。{" "}
+                    </p>
+                  </div>
+                </div>
               </div>
+              {/*end fan*/}
+
+              {/*start fan*/}
+              <div className="lg:mb-32 mb-15">
+                <div className="xl:text-4xl lg:text-3xl md:text-xl font-semibold lg:mb-20 mb-10">
+                  クリエイター
+                </div>
+
+                <div className="lg:mb-20 mb-10">
+                  <div className="xl:text-3xl lg:text-2xl md:text-md font-semibold lg:mb-10 mb-5">
+                    バーチャルPCをつくって応援してもらう
+                  </div>
+
+                  <div className="xl:text-xl lg:text-md text-sm text-sm">
+                    <p>
+                      ステッカーを貼るバーチャルPCを作って、応援をしてもらう準備をしましょう。ファンはシェアしたURLからクリエイターのページを見ることができるので、SNSで共有やプロフィールにURLを載せましょう。。ワンコインから購入して貼ることができます。{" "}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              {/*end fan*/}
+            </div>
+
+            <div className="w-5/12">
+              <Image src="/image/description-illustration.png" width={690} height={722} />
             </div>
           </div>
         </div>
-      </div>
-    </div>
+
+        {/*end description*/}
+
+        <Image src="/decorate/wave-orange2gray.png" width={1920} height={126} />
+      </section>
+
+      {/*TODO: フッター追加*/}
+    </>
   )
 }
 
