6562c7b660681218be0fb7fa2a16f5e2040e4ddf
Stickers-hackathon <watanabeso@crunchtimer.jp>
2021-03-20 09:37:41 +0900

    refs:OGP追加

--- src/pages/testSnsShare.tsx
+++ src/pages/testSnsShare.tsx
@@ -1,20 +1,40 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { SnsShareButton, SnsType } from "../components/Button/snsShare"
+import { Helmet } from "react-helmet"
+import { useRouter } from "next/router"
 
 const TestSnsShareButton: React.FC = () => {
+  // const [setCurrentUrlState, CurrentUrlState] = useState<string>("")
+  const router = useRouter()
+  const baseUrl = "http://example.com"
+  const currentUrl: string = baseUrl + router.pathname
+  const imgUrl = "https://thumb.photo-ac.com/a9/a9e8d7bcc93603347b5ae99dad27538e_t.jpeg"
+
+  useEffect(() => {
+    console.log(location.href)
+  }, [])
+
   return (
     <>
-      <SnsShareButton
-        url={"https://www.google.com/"}
-        snsType={SnsType.Twitter}
-        title={"google検索"}
+      <Helmet
+        title={"Lives&Lives"}
+        meta={[
+          { name: "twitter:card", content: "summary_large_image" },
+          { name: "twitter:site", content: "@IkkoKojima" },
+          { name: "twitter:creator", content: "@IkkoKojima" },
+          { property: "og:title", content: "Lives&Lives" },
+          { property: "og:type", content: "website" },
+          { property: "og:url", content: currentUrl },
+          { property: "og:image", content: imgUrl },
+          { property: "og:description", content: "動物たちのイキイキとした姿をみんなで観察" }
+        ]}
       />
-      <SnsShareButton
-        url={"https://www.google.com/"}
-        snsType={SnsType.Facebook}
-        quote={"google検索"}
-      />
-      <SnsShareButton url={"https://www.google.com/"} snsType={SnsType.Line} title={"google検索"} />
+      <div className={"flex"}>
+        <img src={imgUrl} alt="alt-text" style={{ width: "600px" }} />
+        <SnsShareButton url={currentUrl} snsType={SnsType.Twitter} />
+        <SnsShareButton url={currentUrl} snsType={SnsType.Facebook} quote={"google検索"} />
+        <SnsShareButton url={"https://github.co.jp/"} snsType={SnsType.Line} title={"google検索"} />
+      </div>
     </>
   )
 }
