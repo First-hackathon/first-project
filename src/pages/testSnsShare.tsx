5308b7d44002f51a7181a7a4e1e9f2424f715fc6
Stickers-hackathon <watanabeso@crunchtimer.jp>
2021-03-20 09:48:27 +0900

    refs:OGP追加

--- src/pages/testSnsShare.tsx
+++ src/pages/testSnsShare.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/router"
 const TestSnsShareButton: React.FC = () => {
   // const [setCurrentUrlState, CurrentUrlState] = useState<string>("")
   const router = useRouter()
-  const baseUrl = "http://example.com"
+  const baseUrl = "http://stickers-staging.vercel.app"
   const currentUrl: string = baseUrl + router.pathname
   const imgUrl = "https://thumb.photo-ac.com/a9/a9e8d7bcc93603347b5ae99dad27538e_t.jpeg"
 
