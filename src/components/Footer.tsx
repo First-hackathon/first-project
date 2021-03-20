55db253dc82eb22b6c2d3cbb6a9048a7bece6e1b
TJ-Yusuke <jeeproud@gmail.com>
2021-03-21 01:55:15 +0900

    add: footerスタイル適応

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,18 +2,25 @@ import React from "react"
 export const Footer: React.FC = () => {
   return (
     <>
-      <div className="w-full bg-gray">
-        <img src="" alt="ロゴ" />
-        <p>「小さなステッカーで大きなクリエイター経済を回す」プロジェクト。</p>
+      <div className="w-full bg-lightGray pt-16">
+        <div className="max-w-md mx-auto text-center">
+          {/* TODO: ロゴを追加する */}
+          <img src="" alt="ロゴ" className="mb-6 mx-auto" />
+          <p>「小さなステッカーで大きなクリエイター経済を回す」プロジェクト。</p>
+        </div>
       </div>
-      <div className="w-full bg-orange flex h-14">
-        <p className="text-sm text-white leading-10">&copy; 2021 Sticker. All Rights Reserved.</p>
-        <a href="#" className="text-sm text-white leading-10">
-          プライバシー
-        </a>
-        <a href="#" className="text-sm text-white leading-10">
-          利用規約
-        </a>
+      <div className="w-full bg-orange h-16 py-2.5">
+        <div className="container mx-auto flex justify-between">
+          <p className="text-sm text-white leading-10">&copy; 2021 Sticker. All Rights Reserved.</p>
+          <div className="w-1/4 flex justify-around">
+            <a href="#" className="block text-sm text-white leading-10">
+              プライバシー
+            </a>
+            <a href="#" className="block text-sm text-white leading-10">
+              利用規約
+            </a>
+          </div>
+        </div>
       </div>
     </>
   )
