d0de1142444820cf76e970410ffb49902b0d4420
Stickers-hackathon <watanabeso@crunchtimer.jp>
2021-03-20 13:12:58 +0900

    refs:next　meta追加

--- src/pages/testSnsShare.tsx
+++ src/pages/testSnsShare.tsx
@@ -8,6 +8,8 @@ const TestSnsShareButton: React.FC = () => {
   const baseUrl = "http://stickers-staging.vercel.app"
   const currentUrl: string = baseUrl + router.pathname
   const imgUrl = "https://thumb.photo-ac.com/a9/a9e8d7bcc93603347b5ae99dad27538e_t.jpeg"
+  const imgUrlSecond =
+    "https://dol.ismcdn.jp/mwimgs/a/f/-/img_afa0fad37e6c4d5ce34c01faf54f9e79108563.jpg"
 
   useEffect(() => {
     console.log(location.href)
@@ -19,7 +21,7 @@ const TestSnsShareButton: React.FC = () => {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="twitter:site" content="@sota35163013" />
-        <meta property="og:image" key="ogImage" content={imgUrl} />
+        <meta property="og:image" key="ogImage" content={imgUrlSecond} />
         <meta name="twitter:card" key="twitterCard" content="summary_large_image" />
         <meta name="twitter:image" key="twitterImage" content={currentUrl} />
         <meta name="twitter:title" content="テストテストテストテストテストテスト" />
@@ -27,6 +29,7 @@ const TestSnsShareButton: React.FC = () => {
       </Head>
       <div className={"flex"}>
         <img src={imgUrl} alt="alt-text" style={{ width: "600px" }} />
+        <img src={imgUrlSecond} alt="alt-text" style={{ width: "600px" }} />
         <SnsShareButton url={currentUrl} snsType={SnsType.Twitter} title={"OGP test"} />
         <SnsShareButton url={currentUrl} snsType={SnsType.Facebook} quote={"OGP test"} />
         <SnsShareButton url={currentUrl} snsType={SnsType.Line} title={"OGP test"} />
