8ee806796b0d6b387fa34053df93922fa7331b8c
hourglasshoro <hourglasshoro.628@gmail.com>
2021-03-21 00:44:04 +0900

    Add トップ以外のアカウントボタン色を変更

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,12 @@
 import React from "react"
 import Image from "next/image"
+import { useRouter } from "next/router"
 
 const Header: React.VFC<{}> = () => {
   // TODO: ログインの状態をグローバルから渡す
   const isSignedIn = false
+
+  const router = useRouter()
 
   return (
     <header className="">
@@ -34,7 +37,11 @@ const Header: React.VFC<{}> = () => {
           // TODO: Googleサインインへ遷移
           <a href="">
             <div className="lg:w-16 lg:h-16 w-10 h-10 flex items-center">
-              <Image src="/icon/guest.svg" width={500} height={500} />
+              {router.pathname == "/" ? (
+                <Image src="/icon/guest.svg" width={500} height={500} />
+              ) : (
+                <Image src="/icon/guest-black.svg" width={500} height={500} />
+              )}
             </div>
           </a>
         )}
