6d28a08cae859653efbf080e7c3c835d0ba7b78a
TJ-Yusuke <jeeproud@gmail.com>
2021-03-21 13:58:06 +0900

    add: ユーザーのストライプへの画面遷移

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -6,9 +6,29 @@ import Image from "next/image"
 import Header from "../../components/header"
 import { getUser } from "../../repository/userRepository"
 import { AuthContext } from "../../auth/auth"
+import StripeAPI from "stripe"
+import { useRouter } from "next/router"
+import axios from "axios"
+import { User } from "../../model/user.model"
 
 const Index: React.FC = () => {
   const { currentUser } = useContext(AuthContext)
+
+  const hostName = process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : "http://localhost:3000"
+
+  const router = useRouter()
+
+  // HACK: StripeのSDK(import Stripe from "stripe")でうまく送信できないのでaxiosを使用
+  // StripeのAPI用のインスタンスを生成
+  const axiosInstance = axios.create({
+    baseURL: hostName,
+    headers: {
+      Authorization: `Bearer ${process.env.STRIPE_SECRET_KEY}`
+    },
+    timeout: 2000
+  })
 
   const InputBox = styled.input`
     &::placeholder {
@@ -38,16 +58,74 @@ const Index: React.FC = () => {
   const [profileEditState, setProfileEditState] = useState<boolean>(false)
   const setEditSetter = (isCheckedProfile: boolean) => setProfileEditState(isCheckedProfile)
 
+  const [user, setUser] = useState<User | undefined>()
+
   useEffect(() => {
     if (currentUser) {
       getUser().then((user) => {
         if (user) {
+          setUser(user)
           nameSetInput(user.name)
           setProfileInput(user.profile)
         }
       })
     }
   }, [currentUser])
+
+  // アカウントを作成するためのリンクを取得して遷移させる
+  const createAccount = async () => {
+    try {
+      // アカウント作成
+      const createParams: StripeAPI.AccountCreateParams = {
+        type: "express",
+        country: "JP"
+      }
+
+      let axiosParams = new URLSearchParams()
+      Object.keys(createParams).forEach((key) => {
+        axiosParams.append(key, createParams[key])
+      })
+      let response = await axiosInstance.post("https://api.stripe.com/v1/accounts", axiosParams)
+      const account: StripeAPI.Account = response.data
+      // TODO: 全体を状態管理しているやつにstripeのアカウントIDを保存
+
+      // アカウント登録リンク生成
+      // TODO: refresh_urlとreturn_urlを設定
+      const linkParams: StripeAPI.AccountLinkCreateParams = {
+        account: account.id,
+        refresh_url: `${hostName}/stripe-failed`,
+        return_url: `${hostName}/mypage`,
+        type: "account_onboarding"
+      }
+      axiosParams = new URLSearchParams()
+      Object.keys(linkParams).forEach((key) => {
+        axiosParams.append(key, linkParams[key])
+      })
+      response = await axiosInstance.post("https://api.stripe.com/v1/account_links", axiosParams)
+      const accountLink: StripeAPI.AccountLink = response.data
+
+      // リンクへリダイレクト
+      await router.push(accountLink.url)
+    } catch (e) {
+      console.log(e)
+      // TODO: エラーハンドリング
+    }
+  }
+
+  const getDashboardUrl = async () => {
+    try {
+      // TODO: DBからfirebase idに紐づいているstripe idをとってくる, 下のidは入れ替え
+      const id = "acct_1IWG3iPwhcz9FWG1"
+
+      const response = await axiosInstance.post(
+        `https://api.stripe.com/v1/accounts/${id}/login_links`
+      )
+      await router.push(response.data.url)
+    } catch (e) {
+      console.log(e)
+      // TODO: エラーハンドリング
+    }
+  }
 
   return (
     <>
@@ -100,11 +178,25 @@ const Index: React.FC = () => {
         連携をしてマイPCを作りましょう
       </p>
       <div className={"w-1/4 mx-auto pt-6"}>
-        <RoundedButton
-          size={RoundedDivSize.M}
-          onClick={() => {}}
-          text={"Stripeアカウントを連携する"}
-        />
+        {user ? (
+          user.stripeCustomerId ? (
+            <RoundedButton
+              size={RoundedDivSize.M}
+              onClick={() => {
+                getDashboardUrl()
+              }}
+              text={"管理画面を開く"}
+            />
+          ) : (
+            <RoundedButton
+              size={RoundedDivSize.M}
+              onClick={() => {
+                createAccount()
+              }}
+              text={"Stripeアカウントを連携する"}
+            />
+          )
+        ) : null}
       </div>
     </>
   )
