24284d0b6323ad0c9a7f084b59da3250f6d9812f
hourglasshoro <hourglasshoro.628@gmail.com>
2021-03-21 11:20:21 +0900

    Update Footerを修正

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,20 @@
 import React from "react"
+import Image from "next/image"
 export const Footer: React.FC = () => {
   return (
     <>
       <div className="w-full bg-gray-light pt-16">
-        <div className="max-w-md mx-auto text-center">
-          {/* TODO: ロゴを追加する */}
-          <img src="" alt="ロゴ" className="mb-6 mx-auto" />
+        <div className="max-w-md mx-auto text-center pb-8">
+          <div className="text-center">
+            <div className="lg:w-16 lg:h-16 w-10 h-10 mx-auto">
+              <Image src="/logo/logo.svg" width={80} height={80} />
+            </div>
+
+            <div>
+              <h1 className="lg:text-4xl text-2xl font-semibold">Stickers</h1>
+            </div>
+          </div>
+
           <p className="text-left text-gray">
             「小さなステッカーで大きなクリエイター経済を回す」
             <br />
